1f21e0b877212fccb187cde54daedbb1dd7abde3
Ewerton Batista <ewerton13batista@gmail.com>
2024-04-26 17:53:22 -0300

    Set body as Node2D

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-func _on_body_entered(body):
-	if body.name == "Character":		
+func _on_body_entered(body: Node2D):
+	if body.name == "Character":	
 		body.velocity.y = body.JUMP_FORCE
 		owner.anim.play("hurt")
