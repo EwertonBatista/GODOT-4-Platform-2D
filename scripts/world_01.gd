f3b911bba17904dcb454833c3300353caf0aa71e
Ewerton Batista <ewerton13batista@gmail.com>
2024-05-07 10:32:44 -0300

    Add reload_game function

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -1,8 +1,27 @@
 extends Node2D
 
-
+@onready var control = $HUD/control
+@onready var player_scene = preload("res://actors/character.tscn")
 @onready var player := $Character as CharacterBody2D
 @onready var camera := $Camera2D as Camera2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player.follow_camera(camera)
+	Globals.player = player
+	Globals.player.follow_camera(camera)
+	Globals.player.player_has_died.connect(reload_game)
+	control.time_is_up.connect(reload_game)
+	
+func reload_game():
+	await get_tree().create_timer(1.0).timeout
+	var player = player_scene.instantiate()
+	add_child(player)
+	Globals.player = player
+	Globals.player.follow_camera(camera)
+	Globals.player.player_has_died.connect(reload_game)
+	Globals.coins = 0;
+	Globals.score = 0;
+	Globals.player_life = 3;
+	Globals.respawn_player()
+	#get_tree().reload_current_scene()
+
